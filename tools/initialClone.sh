669bc68b3ea524944bd2368d5477d38754e1d2a4
shadowrunner11 <pieroramirez810@gmail.com>
2023-01-28 19:34:00 -0500

    :tada: initial commit

--- tools/initialClone.sh
+++ tools/initialClone.sh
@@ -4,7 +4,9 @@ if ["$(ls -A $DIR)" ]; then
   echo "Apparently there is a project already created"
 else
   echo "Creating new project"
-  git clone git@github.com:Shadowrunner11/thot-starter.git $DIR
+  git clone https://github.com/Shadowrunner11/thot-starter.git $DIR
   rm -rf $DIR/.git
   rm -rf $DIR/tools
+  git clone git@github.com:Shadowrunner11/thot-frontend.git $DIR/thot-frontend
+  mdkir thot-backend
 fi
