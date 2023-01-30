ec2c3864eb8963ac250c9ac5692d461da06b6066
Piero <pieroramirez810@gmail.com>
2023-01-30 07:17:16 -0500

    Update initialClone.sh

--- tools/initialClone.sh
+++ tools/initialClone.sh
@@ -8,5 +8,5 @@ else
   rm -rf $DIR/.git
   rm -rf $DIR/tools
   git clone git@github.com:Shadowrunner11/thot-frontend.git $DIR/thot-frontend
-  mkdir thot-backend
+  git clone git@github.com:Shadowrunner11/thot-backend.git $DIR/thot-backend
 fi
